5b0a0f28f8bed21233d62e4cb5d853992e054155
QviDev <siva.padmanaban@qlstyle.com>
2022-09-04 15:39:16 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+const app = {
+  title: "React crash course",
+};
+function App() {
+  return (
+    <div className="App">
+      <div className="appContainer">
+        <h3>{app.title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
